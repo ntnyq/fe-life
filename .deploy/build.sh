9bde73823270317d2760986598d76567b44ef7d2
ntnyq <ntnyq13@gmail.com>
2020-01-20 02:17:54 +0800

    build: add build shell

---
+++ .deploy/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WEB_PATH=${dirname $0}
+
+echo "[deploy] Fetch and rebuilding..."
+cd $WEB_PATH
+cd ..
+echo "[deploy] path:" ${pwd}
+echo "[deploy] pulling source code..."
+git fetch --all && git reset --hard origin/master && git pulling
+git checkout master
+echo "[deploy] building start..."
+yarn
+yarn build
+echo "[deploy] building done."
