47fac3b022a9da2026e1f24efab36d489ec7f798
ntnyq <ntnyq13@gmail.com>
2024-12-24 00:58:42 +0800

    chore: update

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,16 +1,12 @@
-/**
- * @file
- */
-
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins: [UnoCSS()],
+
   optimizeDeps: {
     exclude: ['@nolebase/vitepress-plugin-enhanced-readabilities', 'vitepress'],
   },
-
-  plugins: [UnoCSS()],
 
   ssr: {
     noExternal: ['@nolebase/vitepress-plugin-enhanced-readabilities'],
